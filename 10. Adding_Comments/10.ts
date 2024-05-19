5f03e221e8139a68c5c2c223f06169ece66cafa7
SSalmanAli <159621448+SSalmanAli@users.noreply.github.com>
2024-05-19 21:32:14 +0500

    Update 10.ts

--- 10. Adding_Comments/10.ts
+++ 10. Adding_Comments/10.ts
@@ -1,12 +1,13 @@
-// let famous_person = ("Elon musk");
-// let message = ('once said, "Some people don\'t like change, but you need to embrace change if the alternative is disaster."');
-// console.log(famous_person , message)
+let famous_person = ("Elon musk");
+let message = ('once said, "Some people don\'t like change, but you need to embrace change if the alternative is disaster."');
+console.log(famous_person , message)
 
-// console.log (4 + 4);
-// console.log (10 - 2);
-// console.log (4 * 2);
-// console.log (16 / 2);
+console.log (4 + 4); // Does addition 
+console.log (10 - 2); // does subtraction
+console.log (4 * 2); // does multiplication
+console.log (16 / 2); // does division
 
 // name = Salman ali
 
 // date = 10/3/2024 
+
