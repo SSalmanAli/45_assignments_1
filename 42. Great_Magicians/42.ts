aa70b248b6445f460e8b699d5ba0dec2113876ba
SSalmanAli <159621448+SSalmanAli@users.noreply.github.com>
2024-05-19 21:42:24 +0500

    Update 42.ts

--- 42. Great_Magicians/42.ts
+++ 42. Great_Magicians/42.ts
@@ -1,13 +1,8 @@
-let Magicians = ["Roger Lapin" , "David Copperfield" , "Paul Daniels" , "Harry Houdini"];
+function make_great (index : string[] = ["Roger Lapin" , "David Copperfield" , "Paul Daniels" , "Harry Houdini"]
+){
+    index.forEach(y => console.log(`The Great ${y}`))
+}
 
-function show_magicians( Magicians: string[]) {
-    Magicians.forEach(y => console.log(y))
-};
-
-show_magicians(Magicians)
-console.log(`-------------------------------------------------------------`)
-function make_great( Magicians: string[]) {
-    Magicians.forEach(x => console.log(`The Great ${x}`))
-};
-
-make_great(Magicians);
+make_great()
+console.log('----------------------------------')
+make_great(["baka" , "moiz" , "salman"])
