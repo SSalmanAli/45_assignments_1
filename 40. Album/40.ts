66186da63716bfa50b48642f958904578fde10f7
SSalmanAli <159621448+SSalmanAli@users.noreply.github.com>
2024-05-19 21:40:19 +0500

    Update 40.ts

--- 40. Album/40.ts
+++ 40. Album/40.ts
@@ -1,13 +1,20 @@
-function make_album(artist_name: string = "Ali Zafar", album_title: string = "Larsha Peshawar" , tracks:number = 0) {
-    let album = { artist_name , album_title , tracks}
-    return album;
+function make_album (artist_name : string , album_title :string)
+: string;
+function make_album (artist_name : string , album_title :string)
+: string;
+function make_album (artist_name : string , album_title :string)
+: string;
+function make_album (artist_name : string , album_title :string , noOfTracks : number)
+: string;
+
+function make_album(artist_name : string , album_title : string , noOfTracks? : number) : any {
+    if (noOfTracks !== undefined){
+    return { artist_name , album_title , noOfTracks}
+    }
+    else{
+        return { artist_name , album_title }
+    }
 }
 
-console.log(make_album("Atif aslam" , "Adat"));
-console.log(make_album());
-console.log(make_album("Young Stunners" , "Why not meri Jan"));
-
-
-
-
-
+console.log(make_album('Atif aslam', 'Adat' , 13)); // Without tracks
+console.log(make_album('ali zafar', 'larsha peshawar')); // With tracks
